addbcdc3ab07085b9eb228707d0c489961f336f1
Kevin Smith <kevin.ar.smith@gmail.com>
2021-03-18 16:52:20 -0500

    adding

--- data/mystery.sql
+++ data/mystery.sql
@@ -57,6 +57,7 @@ REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
+
 --Verify that information was sucsesssfully uploaded
 select * from departments;
 select * from dept_emp;
@@ -85,13 +86,26 @@ ON dept_manager.emp_no = employees.emp_no;
 --4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, dept_emp.dept_no
 From employees
-JOIN dept_emp ON emp_no= 
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no;
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
+SELECT employees.emp_no, employees.first_name, employees.last_name, employees.sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
+SELECT dept_emp.dept_no, employees.emp_no, employees.last_name, employees.first_name,departments.dept_name
+FROM dept_emp
+INNER JOIN employees ON dept_emp.emp_no = employees.emp_no
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE dept_emp.dept_no = 'd007';
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+SELECT dept_emp.dept_no, employees.emp_no, employees.last_name, employees.first_name,departments.dept_name
+FROM dept_emp
+INNER JOIN employees ON dept_emp.emp_no = employees.emp_no
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE dept_emp.dept_no = 'd007' OR dept_emp.dept_no = 'd005'
+ORDER BY dept_no;
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
 
